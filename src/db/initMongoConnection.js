8496a5adbd39909c79496ec2d7369033f80ba99d
Artur <agavrik464@gmail.com>
2025-02-26 15:23:09 +0200

    1234

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 
 export const initMongoConnection = async () => {
     try {
-        const url = env("MONGODB_URL");
         const db = env("MONGODB_DB");
+        const name = env("MONGODB_USER");
+        const password = env("MONGODB_PASSWORD");
 
         // Строка подключения без дополнительных данных
-        const mongoUri = `${url}/${db}?retryWrites=true&w=majority&appName=${db}`;
+        const mongoUri = `mongodb+srv://${name}:${password}@cluster0.y7kxx.mongodb.net/?retryWrites=true&w=majority&appName=${db}`;
 
         await mongoose.connect(mongoUri);
 
