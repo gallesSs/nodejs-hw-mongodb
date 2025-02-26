8e5460557351d8882cb4d751abb1e9bfc02aefaf
Artur <agavrik464@gmail.com>
2025-02-26 14:55:21 +0200

    new try

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,7 +8,8 @@ export const initMongoConnection = async () => {
         const url = env("MONGODB_URL");
         const db = env("MONGODB_DB");
 
-        await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`);
+await mongoose.connect(`${url}/${db}?retryWrites=true&w=majority`);
+
         console.log("Mongo connection successfully established!");
     } catch (error) {
         console.log("Error while setting up mongo connection", error);
