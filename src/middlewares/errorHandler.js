dff2db4027b4ed9b2d836de675e967fde9c693ff
Artur <agavrik464@gmail.com>
2025-02-28 08:51:15 +0200

    hw3 update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,15 +5,15 @@ export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.name,
-      data: err,
+      message: err.message,
     });
     return;
   }
 
+
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
-    data: err.message,
+    error: err.message,
   });
-}
+};
