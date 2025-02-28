8fc884ffeb114c046c46b0e8e44fa4093b2f691e
Artur <agavrik464@gmail.com>
2025-02-28 08:54:13 +0200

    update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,19 +1,19 @@
-import { HttpError } from 'http-errors';
+import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   // Перевірка, чи отримали ми помилку від createHttpError
-  if (err instanceof HttpError) {
+  if (err instanceof isHttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.message,
+      message: err.name,
+      data: err,
     });
     return;
   }
 
-
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
-    error: err.message,
+    data: err.message,
   });
-};
+}
