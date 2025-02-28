59986b059c28c1550591d7cf56cbfd3ac914c77c
Artur <agavrik464@gmail.com>
2025-02-28 21:51:22 +0200

    some update

--- src/db/validation/contact.js
+++ src/db/validation/contact.js
@@ -19,7 +19,7 @@ export const createContactSchema = Joi.object({
         'string.min': 'Email should have at least {#limit} characters',
         'string.max': 'Email should have at most {#limit} characters',
     }),
-    isFavorite: Joi.boolean(),
+    isFavourite: Joi.boolean(),
     contactType: Joi.string().valid("work", "home", "personal").required().messages({
         'any.required': 'Type of number is required',
     }),
@@ -42,6 +42,6 @@ export const updateContactSchema = Joi.object({
         'string.min': 'Email should have at least {#limit} characters',
         'string.max': 'Email should have at most {#limit} characters',
     }),
-    isFavorite: Joi.boolean(),
+    isFavourite: Joi.boolean(),
     contactType: Joi.string().valid("work", "home", "personal"),
 });
