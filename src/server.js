ea43d1eb4a28aa6e2134d11160f990b1d60661f8
Artur <agavrik464@gmail.com>
2025-03-02 10:40:57 +0200

    updated libs

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ const PORT = Number(env(ENV_VARS.PORT, "3000"));
 
 export const setupServer = () => {
     const app = express();
-  app.use(express.json());
+    app.use(express.json());
+    app.use(cookieParser());
     app.use(cors());
     app.use(pino({
         transport: {
